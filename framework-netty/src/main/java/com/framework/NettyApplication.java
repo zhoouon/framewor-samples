bad82bcbfb6cc572a85285b8d21e2602c1bd5ad9
zhoudong <zhoouon@126.com>
2024-10-13 13:33:20 +0800

    添加netty模块

---
+++ framework-netty/src/main/java/com/framework/NettyApplication.java
@@ -0,0 +1,13 @@
+package com.framework;
+
+/**
+ * @Author: zhoudong
+ * @Description: TODO
+ * @Date: 2024/10/13 13:26
+ * @Version: 1.0.0
+ **/
+public class NettyApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
