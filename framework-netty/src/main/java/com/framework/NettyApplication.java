fcddb0de8bd5f098f88ee4c9d26635b389e6dd68
zhoudong <zhoouon@126.com>
2024-10-27 15:01:57 +0800

    优化日志打印

--- framework-netty/src/main/java/com/framework/NettyApplication.java
+++ framework-netty/src/main/java/com/framework/NettyApplication.java
@@ -1,13 +1,17 @@
 package com.framework;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: zhoudong
  * @Description: TODO
  * @Date: 2024/10/13 13:26
  * @Version: 1.0.0
  **/
+@SpringBootApplication
 public class NettyApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(NettyApplication.class, args);
     }
 }
